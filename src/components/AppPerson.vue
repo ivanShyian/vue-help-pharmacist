b5079e6bf37573d84040e6674de1e2bc0af975fc
vanjkes <shyian.ivan@yahoo.com>
2021-03-06 00:18:52 +0200

    Little bit fixed css styles

--- src/components/AppPerson.vue
+++ src/components/AppPerson.vue
@@ -50,22 +50,26 @@ export default {
 .person-card {
   position: absolute;
   top: 10%;
-  left: 50%;
-  display: flex;
-  flex-direction: column;
+  left: 44%;
   box-shadow: 0 0 40px rgba(127, 127, 127, 0.4);
   border-radius: 40px;
-  max-width: 550px;
-  height: 480px;
+  height: 550px;
   background: white;
+  max-width: 550px;
+  @media (min-width: 1700px) {
+    left: 44%;
+    max-width: 650px;
+    height: 650px;
+  }
   @media (min-width: 2000px) {
     border-radius: 60px;
-  }
-  @media (min-width: 2000px) {
     max-width: 750px;
+    height: 750px;
+    left: 50%;
   }
-  @media (max-height: 900px) {
+  @media (max-height: 800px) {
     max-width: 400px;
+    height: 400px;
   }
 
   &__image {
